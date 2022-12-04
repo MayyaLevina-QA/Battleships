82abee11fe3f01a40c796de7a3f65f804d589784
MayyaLevina-QA <116386796+MayyaLevina-QA@users.noreply.github.com>
2022-12-04 14:37:34 +0300

    Update battleship.js

--- battleship.js
+++ battleship.js
@@ -13,7 +13,7 @@ while (isSunk == false) {
     } else {
         guesses = guesses + 1;
         if (guess == location1 || guess == location2 || guess == location3) {
-            alert("HIT!");
+            alert("Попал!");
             hits = hits + 1;
             if (hits == 3) {
               isSunk = true;
